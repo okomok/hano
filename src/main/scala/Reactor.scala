525fbba5aff9645764a97683075096c17a6461fe
unknown <okomok@gmail.com>
2010-12-05 06:06:38 +0900

    startReactive -> startHano

--- src/main/scala/Reactor.scala
+++ src/main/scala/Reactor.scala
@@ -13,25 +13,22 @@ import scala.actors.Actor
 
 
 /**
- * The Seq Actor
+ * An actor built upon Seq
  */
 trait Reactor extends Actor {
     /**
      * Override to build up a Seq.
      */
-    protected def startReactive(r: Seq[Any]): Unit
+    protected def startHano(r: Seq[Any]): Unit
 
     private var _f: Any => Unit = null // primary
     private var _k: Exit => Unit = null
     private val _fs = new CopyOnWriteArrayList[Any => Unit] // secondaries
-//    private val _ks = new CopyOnWriteArrayList[eval.ByName[Unit]]
 
     final override def act = {
         Actor.loop {
             react {
                 case Reactor.Exit => {
-//                    _k()
-//                    iterative.from(_ks).foreach{k => k()}
                     Actor.exit
                 }
                 case x => {
@@ -47,20 +44,20 @@ trait Reactor extends Actor {
     }
 
     final override def exceptionHandler = {
-        if (_k == null) {
-            super.exceptionHandler
-        } else {
+        if (_k != null) {
             case t => _k(Exit.Thrown(t))
+        } else {
+            super.exceptionHandler
         }
     }
 
     final override def start = {
-        startReactive(new Reactor.Primary(this))
+        startHano(new Reactor.Primary(this))
         super.start
     }
 
     final override def restart = {
-        startReactive(new Reactor.Primary(this))
+        startHano(new Reactor.Primary(this))
         super.restart
     }
 }
@@ -83,7 +80,7 @@ object Reactor {
      */
     def apply(f: Seq[Any] => Unit = Starter): Reactor = {
         val a = new Reactor {
-            override protected def startReactive(r: Seq[Any]) = f(r)
+            override protected def startHano(r: Seq[Any]) = f(r)
         }
         a.start
         a
@@ -94,7 +91,7 @@ object Reactor {
      */
     def singleThreaded(f: Seq[Any] => Unit = Starter): Reactor = {
         val a = new Reactor {
-            override protected def startReactive(r: Seq[Any]) = f(r)
+            override protected def startHano(r: Seq[Any]) = f(r)
             override def scheduler = new scala.actors.scheduler.SingleThreadedScheduler
         }
         a.start
@@ -105,7 +102,7 @@ object Reactor {
         override def apply(r: Seq[Any]) = r.start
     }
 
-    private final class Wrap(f: Any => Unit) extends (Any => Unit) {
+    private class Wrap(f: Any => Unit) extends (Any => Unit) {
         override def apply(x: Any) = f(x)
     }
 
@@ -118,16 +115,12 @@ object Reactor {
 
     private[hano] class Secondary(_1: Reactor) extends Resource[Any] {
         private[this] var _f: Any => Unit = null
-//        private[this] var _k: eval.ByName[Unit] = null
         override protected def closeResource() {
             _1._fs.remove(_f)
-//            _1._ks.remove(_k)
         }
         override protected def openResource(f: Any => Unit, k: Exit => Unit) {
             _f = new Wrap(f)
-//            _k = eval.ByName(k)
             _1._fs.add(_f)
-//            _1._ks.add(_k)
         }
     }
 
