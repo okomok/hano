da9f611ac1d311a213b9fef9e8c49332c4f52204
unknown <okomok@gmail.com>
2010-12-26 09:58:13 +0900

    Sync algorithms

---
+++ src/main/scala/Sync.scala
@@ -0,0 +1,123 @@
+
+
+// Copyright Shunsuke Sogame 2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok
+package hano
+
+
+import detail.For
+
+
+/**
+ * Contains synchronous algorithms.
+ */
+object Sync {
+
+    final class Val[A] extends Reaction[A] with Reaction.Checked[A] { self =>
+        private[this] var v: Either[Throwable, A] = null
+        private[this] val c = new java.util.concurrent.CountDownLatch(1)
+
+        override protected def applyChecked(x: A) {
+            if (v != null) {
+                return
+            }
+            try {
+                v = Right(x)
+            } finally {
+                c.countDown
+            }
+        }
+        override protected def exitChecked(q: Exit) {
+            try {
+                q match {
+                    case Exit.Failed(t) => if (v == null) { v = Left(t) }
+                    case _ => ()
+                }
+            } finally {
+                c.countDown
+            }
+        }
+
+        def apply(): A = {
+            c.await()
+            if (v == null) {
+                throw new NoSuchElementException("Sync.Val.apply()")
+            }
+            v match {
+                case Right(x) => x
+                case Left(t) => throw t
+            }
+        }
+
+        def toFunction = new Function0[A] {
+            override def apply = self()
+        }
+    }
+
+    def head[A](xs: Seq[A]): Function0[A] = {
+        val v = new Val[A]
+        var go = true
+        For(xs) { x =>
+            if (go) {
+                go = false
+                v(x)
+                xs.close()
+            }
+        } AndThen {
+            v.exit(_)
+        }
+        v.toFunction
+    }
+
+    def last[A](xs: Seq[A]): Function0[A] = {
+        val v = new Val[A]
+        var acc: Option[A] = None
+        For(xs) { x =>
+            acc = Some(x)
+        } AndThen {
+            case Exit.End => if (acc.isEmpty) v.exit(Exit.End) else v(acc.get)
+            case q => v.exit(q)
+        }
+        v.toFunction
+    }
+
+    def foldLeft[A, B](xs: Seq[A])(z: B)(op: (B, A) => B): Function0[B] = {
+        val v = new Val[B]
+        var acc = z
+        For(xs) { x =>
+            acc = op(acc, x)
+        } AndThen {
+            case Exit.End => v(acc)
+            case q => v.exit(q)
+        }
+        v.toFunction
+    }
+
+    def reduceLeft[A](xs: Seq[A])(op: (A, A) => A): Function0[A] = {
+        val v = new Val[A]
+        var acc: Option[A] = None
+        For(xs) { x =>
+            if (acc.isEmpty) {
+                acc = Some(x)
+            } else {
+                acc = Some(op(acc.get, x))
+            }
+        } AndThen {
+            case Exit.End => if (acc.isEmpty) v.exit(Exit.End) else v(acc.get)
+            case q => v.exit(q)
+        }
+        v.toFunction
+    }
+
+    def min[A](xs: Seq[A])(implicit c: Ordering[A]): Function0[A] = {
+        reduceLeft(xs)(c.min(_, _))
+    }
+
+    def max[A](xs: Seq[A])(implicit c: Ordering[A]): Function0[A] = {
+        reduceLeft(xs)(c.max(_, _))
+    }
+
+}
---
+++ src/test/scala/SyncTest.scala
@@ -0,0 +1,99 @@
+
+
+// Copyright Shunsuke Sogame 2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.hanotest
+
+
+import com.github.okomok.hano
+
+
+class SyncTest extends org.scalatest.junit.JUnit3Suite {
+
+    def testMinMax {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq(6,3,1,2,6,7,4,2))
+        val (ys, zs) = xs.duplicate
+        val max = hano.Sync.max(ys)
+        val min = hano.Sync.min(zs)
+        expect(7)(max())
+        expect(1)(min())
+    }
+
+    def testHead {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq(9,3,1,2,6,7,4,2))
+        val head = hano.Sync.head(xs)
+        expect(9)(head())
+    }
+
+    def testNoHead {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq[Int]())
+        val head = hano.Sync.head(xs)
+        intercept[NoSuchElementException] {
+            head()
+        }
+    }
+
+    def testLast {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq(3,1,2,6,7,4,2,9))
+        val last = hano.Sync.last(xs)
+        expect(9)(last())
+    }
+
+    def testNoLast {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq[Int]())
+        val last = hano.Sync.last(xs)
+        intercept[NoSuchElementException] {
+            last()
+        }
+    }
+
+    def testFoldLeft {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq(3,1,2,6,7,4,2,9))
+        val fold = hano.Sync.foldLeft(xs)("0")(_ + _.toString)
+        expect("031267429")(fold())
+    }
+
+    def testFoldLeftEmpty {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq[Int]())
+        val fold = hano.Sync.foldLeft(xs)("0")(_ + _.toString)
+        expect("0")(fold())
+
+    }
+
+    def testReduceLeft {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq(3,1,2,6,7,4,2,9))
+        val reduce = hano.Sync.reduceLeft(xs)(_ + _)
+        expect(3+1+2+6+7+4+2+9)(reduce())
+    }
+
+    def testReduceLeftEmpty {
+        val xs = hano.Seq.origin(hano.eval.Threaded).generate(Seq[Int]())
+        val reduce = hano.Sync.reduceLeft(xs)(_ + _)
+        intercept[NoSuchElementException] {
+            reduce()
+        }
+    }
+
+    def testValThreaded {
+        val v = new hano.Sync.Val[Int]
+        hano.Seq.origin(hano.eval.Threaded).generate(0 until 10).forloop(v)
+        expect(0)(v())
+    }
+
+    def testValStrict {
+        val v = new hano.Sync.Val[Int]
+        hano.Seq.origin(hano.eval.Strict).generate(0 until 10).forloop(v)
+        expect(0)(v())
+    }
+
+    def testValEmpty {
+        val v = new hano.Sync.Val[Int]
+        hano.Seq.origin(hano.eval.Threaded).generate(Seq()).forloop(v)
+        intercept[NoSuchElementException] {
+            v()
+        }
+    }
+
+}
