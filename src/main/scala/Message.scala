6c802902055d626175bd492145b414781b47397b
unknown <okomok@gmail.com>
2011-01-24 16:21:56 +0900

    Message

---
+++ src/main/scala/Message.scala
@@ -0,0 +1,14 @@
+
+
+// Copyright Shunsuke Sogame 2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok
+package hano
+
+
+/**
+ * Marker trait for actor message
+ */
+trait Message
