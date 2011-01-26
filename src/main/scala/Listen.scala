0463cf8e1318f817fa585d23b206fa16c331f6e9
unknown <okomok@gmail.com>
2011-01-26 13:21:01 +0900

    multiple addBy/removeBy

--- src/main/scala/Listen.scala
+++ src/main/scala/Listen.scala
@@ -8,48 +8,73 @@ package com.github.okomok
 package hano
 
 
+import java.util.ArrayList
+
+
 object Listen {
 
     sealed abstract class Env[A] extends Reaction[A] {
+        /**
+         * How to add a listener
+         */
         def addBy(add: => Unit): Unit
+
+        /**
+         * How to remove a listener
+         */
         def removeBy(remove: => Unit): Unit
     }
 
+    /**
+     * Creates a Seq from listeners.
+     */
     def apply[A](ctx: Context = Context.act)(body: Env[A] => Unit): Seq[A] = {
         val env = new EnvImpl[A](ctx)
         body(env)
-        env.toSeq
+        env.asSeq
     }
 
     private class EnvImpl[A](ctx: Context) extends Env[A] with CheckedReaction[A] {
-
         @volatile private[this] var _f: Reaction[A] = null
-        @volatile private[this] var _add: () => Unit = null
-        @volatile private[this] var _remove: () => Unit = null
+        private[this] var _adds = new ArrayList[() => Unit]
+        private[this] var _removes = new ArrayList[() => Unit]
+        private[this] val _k = detail.ExitOnce { q => _f.exit(q); asSeq.close() }
 
         override def checkedApply(x: A) {
-            ctx.eval {
-                _f(x)
-            }
+            ctx onEach { _ =>
+                _k.beforeExit {
+                    _f(x)
+                }
+            } onExit {
+                case q @ Exit.Failed(t) => _k(q)
+                case _ => ()
+            } start()
         }
         override def checkedExit(q: Exit) {
+            asSeq.close()
         }
 
         override def addBy(add: => Unit) {
-            _add = () => add
+            _adds.add(() => add)
         }
         override def removeBy(remove: => Unit) {
-            _remove = () => remove
+            _removes.add(() => remove)
         }
 
-        def toSeq: Seq[A] = new Resource[A] {
-            assert(_add != null)
-            assert(_remove != null)
+        lazy val asSeq: Seq[A] = new Resource[A] {
             override def context = ctx
-            override protected def closeResource() = _remove()
+            override protected def closeResource() {
+                for (remove <- Iter.from(_removes).able) {
+                    remove()
+                }
+                _removes = null
+            }
             override protected def openResource(f: Reaction[A]) {
-                _f = f
-                _add()
+                _f = f // synchronization point
+                for (add <- Iter.from(_adds).able) {
+                    add()
+                }
+                _adds = null
             }
         }
     }
