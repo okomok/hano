2d720975a223ffea2a724fdf3b3e4fad8f60b3bf
unknown <okomok@gmail.com>
2011-01-27 19:15:30 +0900

    Rist reborns.

---
+++ src/main/scala/Rist.scala
@@ -0,0 +1,62 @@
+
+
+// Copyright Shunsuke Sogame 2010-2011.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok
+package hano
+
+
+/**
+ * Immutable(single-forloop) infinite list
+ */
+final class Rist[A](override val context: Context = Context.act) extends Resource[A] {
+    require(context ne Context.self)
+
+    @volatile private[this] var isActive = true
+    @volatile private[this] var g: Reaction[A] = null
+    private[this] val vs = new java.util.concurrent.ConcurrentLinkedQueue[A]
+    private[this] val _k = detail.ExitOnce { q => g.exit(q); close() }
+
+    override protected def closeResource() {
+        isActive = false
+        vs.clear()
+    }
+    override protected def openResource(f: Reaction[A]) {
+        g = f
+        while (!vs.isEmpty) {
+            val v = vs.poll
+            if (v != null) {
+                eval(f, v)
+            }
+        }
+    }
+
+    def add(x: A) {
+        if (isActive) {
+            if (g != null) {
+                eval(g, x)
+            } else {
+                vs.offer(x)
+                if (g != null && vs.remove(x)) {
+                    eval(g, x)
+                }
+            }
+        }
+    }
+
+    @Annotation.aliasOf("add")
+    def +=(x: A): Unit = add(x)
+
+    private def eval(f: Reaction[A], x: A) {
+        context onEach { _ =>
+            _k.beforeExit {
+                f(x)
+            }
+        } onExit {
+            case q @ Exit.Failed(t) => _k(q)
+            case _ => ()
+        } start()
+    }
+}
---
+++ src/test/scala/RistTest.scala
@@ -0,0 +1,65 @@
+
+
+// Copyright Shunsuke Sogame 2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.hanotest
+
+
+import com.github.okomok.hano
+
+class RistTest extends org.scalatest.junit.JUnit3Suite {
+
+    def testTrivial {
+        val ctx = hano.Context.act
+        val xs = new hano.Rist[Int](ctx)
+
+        val suite = new ParallelSuite(10)
+        val i = new java.util.concurrent.atomic.AtomicInteger(0)
+        suite.add(50) {
+            xs add i.incrementAndGet
+        }
+
+        val q = new java.util.concurrent.ConcurrentLinkedQueue[Int]
+        suite.add(1) {
+            for (x <- xs) {
+                q.offer(x)
+            }
+        }
+
+        suite.start()
+
+        Thread.sleep(2000)
+
+        val arr = new java.util.ArrayList[Int]
+        for (x <- hano.Iter.from(q).able) {
+            arr.add(x)
+        }
+        java.util.Collections.sort(arr, implicitly[Ordering[Int]])
+        expect(hano.Iter.from(1 to 50))(hano.Iter.from(arr))
+    }
+
+    def testTrivial2 {
+        val ctx = hano.Context.act
+        val xs = new hano.Rist[Int](ctx)
+
+        val suite = new ParallelSuite(10)
+        val i = new java.util.concurrent.atomic.AtomicInteger(0)
+        suite.add(50) {
+            xs add 9
+        }
+
+        val q = new java.util.concurrent.ConcurrentLinkedQueue[Int]
+        suite.add(1) {
+            for (x <- xs.take(12)) {
+                q.offer(x)
+            }
+        }
+
+        suite.start()
+
+        Thread.sleep(2000)
+        expect(hano.Iter(9,9,9,9,9,9,9,9,9,9,9,9))(hano.Iter.from(q))
+    }
+}
