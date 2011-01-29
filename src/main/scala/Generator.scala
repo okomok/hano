46dbabafe182527e6028311a314d9eb49d8c442e
unknown <okomok@gmail.com>
2011-01-29 10:37:18 +0900

    Generator cleanup

--- src/main/scala/Generator.scala
+++ src/main/scala/Generator.scala
@@ -8,59 +8,44 @@ package com.github.okomok
 package hano
 
 
-import scala.util.continuations.{suspendable, cpsParam}
+import scala.util.continuations.{cpsParam, suspendable}
 
 
 object Generator {
-
 
     /**
      * Yielding thread is blocked.
      */
     object Sync {
-
         type Env[A] = Reaction[A] with java.io.Flushable
-
-        def apply[A](body: Env[A] => Unit): Iterable[A] = {
-            new detail.SyncIterable(new BodyToSeq(body.asInstanceOf[Reaction[A] => Unit]))
-        }
+        def apply[A](body: Env[A] => Unit): Iterable[A] = new detail.SyncIterable(new BodyToSeq(body))
     }
-
 
     /**
      * Yielding thread is not blocked; elements are buffered.
      */
     object Async {
-
         type Env[A] = Reaction[A]
-
-        def apply[A](body: Env[A] => Unit): Iterable[A] = {
-            new detail.AsyncIterable(new BodyToSeq(body))
-        }
+        def apply[A](body: Env[A] => Unit): Iterable[A] = new detail.AsyncIterable(new BodyToSeq(body))
     }
-
 
     /**
      * Single-threaded.
      */
     object Cps {
-        def apply[A](body: Env[A] => Any @suspendable): Iterable[A] = {
-            new detail.CpsIterable(body)
-        }
-
         abstract class Env[A] extends Block.Env1 {
             def apply(x: A): Unit @suspendable
             def amb[B](xs: Iter[B]): B @cpsParam[Any, Unit]
         }
+        def apply[A](body: Env[A] => Any @suspendable): Iterable[A] = new detail.CpsIterable(body)
     }
 
-
-    private class BodyToSeq[A](body: Reaction[A] => Unit) extends Seq[A] {
+    private class BodyToSeq[A](_1: Reaction[A] with java.io.Flushable => Unit) extends Seq[A] {
         override def context = Self
         override def forloop(f: Reaction[A]) {
             val g = new MultiExitable(f)
             try {
-                body(g)
+                _1(g)
                 g.exit(Exit.End)
             } catch {
                 case t: Throwable => g.exitNothrow(Exit.Failed(t))
@@ -69,18 +54,17 @@ object Generator {
     }
 
     // Accepts multiple exit calls, though it's illegal.
-    private class MultiExitable[A](f: Reaction[A]) extends Reaction[A] with java.io.Flushable {
+    private class MultiExitable[A](_1: Reaction[A]) extends Reaction[A] with java.io.Flushable {
         private[this] var exited = false
-        override def flush() {
-            if (f.isInstanceOf[java.io.Flushable]) {
-                f.asInstanceOf[java.io.Flushable].flush()
-            }
+        override def flush() = _1 match {
+            case f: java.io.Flushable => f.flush()
+            case _ => ()
         }
-        override def apply(x: A) = f(x)
+        override def apply(x: A) = _1(x)
         override def exit(q: Exit) {
             if (!exited) {
                 exited = true
-                f.exit(q)
+                _1.exit(q)
             } else {
                 q match {
                     case Exit.Failed(t) => detail.LogErr(t, "abandoned exception in Generator")
