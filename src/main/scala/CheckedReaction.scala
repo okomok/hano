b8c5f4de2a672651fd5f1e58b43f9acca76d2974
unknown <okomok@gmail.com>
2011-02-09 02:59:49 +0900

    Installs Modification.

--- src/main/scala/CheckedReaction.scala
+++ src/main/scala/CheckedReaction.scala
@@ -8,53 +8,33 @@ package com.github.okomok
 package hano
 
 
-import java.util.ConcurrentModificationException
-
-
-class NotSerializedException[A](reaction: Reaction[A]) extends ConcurrentModificationException("method calls shall be serialized")
-class MultipleExitsException[A](reaction: Reaction[A]) extends ConcurrentModificationException("multiple `exit` calls not allowed")
-class ApplyAfterExitException[A](reaction: Reaction[A]) extends ConcurrentModificationException("`apply` shall not be called after exit")
-
-
 /**
  * Kicks non-conforming Seq.
  */
 @Annotation.mixin
 trait CheckedReaction[-A] extends Reaction[A] {
+
     protected def checkedApply(x: A): Unit
     protected def checkedExit(q: Exit): Unit
 
-    @volatile private var _ing = false
-    private val _exit = {
+    private[this] lazy val mdf = new detail.Modification("method calls shall be serialized in " + toString)
+
+    private[this] val _exit = {
         detail.IfFirst[Exit] { q =>
             checkedExit(q)
         } Else { _ =>
-            throw new MultipleExitsException(this)
+            throw new java.util.ConcurrentModificationException("multiple `exit` calls not allowed in " + toString)
         }
     }
+
     private def _apply(x: A) {
         if (_exit.isSecond) {
-            throw new ApplyAfterExitException(this)
+            throw new java.util.ConcurrentModificationException("`apply` shall not be called after `exit` in " + toString)
         } else {
             checkedApply(x)
         }
     }
 
-    final override def apply(x: A) {
-        if (_ing) {
-            throw new NotSerializedException(this)
-        }
-        try {
-            _ing = true
-            _apply(x)
-        } finally {
-            _ing = false
-        }
-    }
-    final override def exit(q: Exit) = {
-        if (_ing) {
-            throw new NotSerializedException(this)
-        }
-        _exit(q)
-    }
+    final override def apply(x: A) = mdf { _apply(x) }
+    final override def exit(q: Exit) = mdf { _exit(q) }
 }
