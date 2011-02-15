9fea4476682f6d28ae64f5527ed6f4a917b2353a
unknown <okomok@gmail.com>
2011-02-15 11:52:43 +0900

    typo

--- src/main/scala/detail/DropUntil.scala
+++ src/main/scala/detail/DropUntil.scala
@@ -12,7 +12,7 @@ package detail
 private[hano]
 class DropUntil[A](_1: Seq[A], _2: Seq[_]) extends Seq[A] {
     override def close() = { _1.close(); _2.close() }
-    override def context = _1.context upper _2.context
+    override def context = _1.context
     override def forloop(f: Reaction[A]) {
         @volatile var go = false
 
