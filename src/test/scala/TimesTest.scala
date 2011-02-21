6a5b42fa3d31bca7e6319e46066a854c22f9e2c9
unknown <okomok@gmail.com>
2011-02-22 07:29:32 +0900

    empty test

--- src/test/scala/TimesTest.scala
+++ src/test/scala/TimesTest.scala
@@ -11,7 +11,7 @@ import com.github.okomok.hano
 
 class TimesTest extends org.scalatest.junit.JUnit3Suite {
 
-    def teztTrivialSelf {
+    def testTrivialSelf {
         val xs = hano.Self.loop.pull(1 until 4).times(4)
         expect(hano.Iter(1,2,3,1,2,3,1,2,3,1,2,3))(xs.toIter)
     }
@@ -19,5 +19,15 @@ class TimesTest extends org.scalatest.junit.JUnit3Suite {
     def testTrivialAsync {
         val xs = hano.async.loop.pull(1 until 4).times(4)
         expect(hano.Iter(1,2,3,1,2,3,1,2,3,1,2,3))(xs.toIter)
+    }
+
+    def testTrivialSelfEmpty {
+        val xs = hano.Self.loop.pull(1 until 4).times(0)
+        expect(hano.Iter())(xs.toIter)
+    }
+
+    def testTrivialAsyncEmpty {
+        val xs = hano.async.loop.pull(1 until 4).times(0)
+        expect(hano.Iter())(xs.toIter)
     }
 }
