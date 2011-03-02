0328bee1497595e8883b05e15cc0aceb6ae7895e
unknown <okomok@gmail.com>
2011-03-02 16:56:06 +0900

    Revert LoopWhileSelf

--- src/main/scala/detail/LoopWhile.scala
+++ src/main/scala/detail/LoopWhile.scala
@@ -11,8 +11,6 @@ package detail
 
 private[hano]
 class LoopWhile[A](_1: Seq[A], _2: () => Boolean) extends SeqProxy[A] {
-    override val self = new LoopWhileOther(_1, _2)
-/*
     override val self = {
         if (_1.context eq Self) {
             new LoopWhileSelf(_1, _2)
@@ -20,20 +18,19 @@ class LoopWhile[A](_1: Seq[A], _2: () => Boolean) extends SeqProxy[A] {
             new LoopWhileOther(_1, _2)
         }
     }
-*/
 }
 
 
 private[hano]
 class LoopWhileOther[A](_1: Seq[A], _2: () => Boolean, grainSize: Int = 1) extends SeqAdapter.Of[A](_1) {
-    //assert(_1.context ne Self)
+    assert(_1.context ne Self)
 
     override def forloop(f: Reaction[A]) {
         @volatile var status = Exit.Success.asStatus
         @volatile var isActive = true
 
         def rec() {
-            _1 onEnter { p =>
+            _1.onEnter { p =>
                 f.enter {
                     Exit { q =>
                         p(q)
@@ -75,46 +72,42 @@ class LoopWhileOther[A](_1: Seq[A], _2: () => Boolean, grainSize: Int = 1) exten
     }
 }
 
-/*
+
 // Specialized to avoid stack-overflow.
 private[hano]
-class LoopWhileSelf[A](_1: Seq[A], _2: () => Boolean) extends Seq[A] {
+class LoopWhileSelf[A](_1: Seq[A], _2: () => Boolean) extends SeqAdapter.Of[A](_1) {
     assert(_1.context eq Self)
 
-    @volatile private[this] var isActive = false
-    override val context = _1.context.toKnown
-    override def close() { isActive = false; _1.close() }
-
-    // requires synchronized in case close-then-forloop from other threads.
-    override def forloop(f: Reaction[A]) = synchronized {
-        assert(!isActive)
-
-        isActive = true
-        def _k(q: Exit.Status) { close(); f.exit(q) }
+    override def forloop(f: Reaction[A]) {
+        @volatile var status = Exit.Success.asStatus
+        @volatile var isActive = true
 
         @scala.annotation.tailrec
         def rec() {
-            if (!_2()) {
-                context eval {
-                    _k(Exit.Success)
+            _1.noSuccess.onEnter { p =>
+                f.enter {
+                    Exit { q =>
+                        p(q)
+                        status = Exit.Failure(Exit.ByOther(q))
+                        isActive = false
+                    }
                 }
-                return
-            }
-
-            _1.noSuccess onEach {
+                if (!_2()) {
+                    f.exit(Exit.Success)
+                }
+            } onEach {
                 f(_)
             } onExit {
-                _k(_)
+                f.exit(_)
             } start()
 
             if (isActive) {
                 rec()
             } else {
-                _k(Exit.Closed)
+                f.exit(status)
             }
         }
 
         rec()
     }
 }
-*/
